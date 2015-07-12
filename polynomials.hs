e5e83c386ec74cb2b0cadb3102d55e098c42cffd
Dow Drake <dowdrake@msn.com>
2015-07-11 19:26:39 -0700

    added a way to perform simplifying substitutions and construct a multiplication table of the field generated by Z3 and a root of a given minimum polynomial in that root over Z3.  ref. Pinter ch 27

--- polynomials.hs
+++ polynomials.hs
@@ -1,5 +1,6 @@
 import Data.List
 import qualified Data.Map as M
+import System.IO
 
 -- RESULTS:
 -- Constructing each of the p^(n+1) possible polynomials of degree n or lower
@@ -10,9 +11,9 @@ import qualified Data.Map as M
 -- example: In Z5, the 5^5 possible polynomials of degree 4 or lower are all distinct functions 
 -- and hence span the entire space of 5^5 possible graphs
 
---*Main> let ms = mranges coeff55 [0..4]
---*Main> length . nub $ ms
---3125
+-- *Main> let ms = mranges coeff55 [0..4]
+-- *Main> length . nub $ ms
+-- 3125
 
 
 -- It seems clear that this nice result comes from the fact that p is prime so Zp is a field
@@ -20,15 +21,15 @@ import qualified Data.Map as M
 -- we find only 3*6^2 = 108 distinct graphs.
 -- Each of these graphs is common to 2*6^3 = 432 distinct polynomials
 
---*Main> :l polynomials.hs 
---[1 of 1] Compiling Main             ( polynomials.hs, interpreted )
---Ok, modules loaded: Main.
---*Main> let ms = mranges coeff66 [0..5]
---*Main> let hs = histo ms coeff66
---*Main> let mhs = M.fromList hs
---*Main> M.size mhs
---108
---*Main> M.toList mhs
+-- *Main> :l polynomials.hs 
+-- [1 of 1] Compiling Main             ( polynomials.hs, interpreted )
+-- Ok, modules loaded: Main.
+-- *Main> let ms = mranges coeff66 [0..5]
+-- *Main> let hs = histo ms coeff66
+-- *Main> let mhs = M.fromList hs
+-- *Main> M.size mhs
+-- 108
+-- *Main> M.toList mhs
 -- [([0,0,0,0,0,0],432),([0,0,2,0,0,2],432),([0,0,4,0,0,4],432),([0,1,0,3,4,3],432),([0,1,2,3,4,5],432),([0,1,4,3,4,1],432),
 --  ([0,2,0,0,2,0],432),([0,2,2,0,2,2],432),([0,2,4,0,2,4],432),([0,3,0,3,0,3],432),([0,3,2,3,0,5],432),([0,3,4,3,0,1],432),
 --  ([0,4,0,0,4,0],432),([0,4,2,0,4,2],432),([0,4,4,0,4,4],432),([0,5,0,3,2,3],432),([0,5,2,3,2,5],432),([0,5,4,3,2,1],432),
@@ -75,9 +76,79 @@ histo bins vals = let nb = nub bins
                       zbv  = zip bins vals
                   in [(b, length . filter (\(bn,v) -> bn == b) $ zbv) | b <- bins]
 
+-- given two polynomials with the same number of coeffs (some coeffs may be zero so degrees can be different)
+-- compute their product in Zn
+polymult :: Int -> [Int] -> [Int] -> [Int]
+polymult n p1 p2 = let d = length p1 - 1
+                       coeffm n m p1 p2 = sum [(p1!!x * p2!!(m-x)) | x <- [max 0 (m-d)..min m d]] `rem` n
+                   in [coeffm n m p1 p2 | m <- [0..2*d]]
+
+-- construct the multiplication table for a field of polynomials over Zn
+multtable :: Int -> [[Int]] -> [[String]]
+multtable n coeffs = [[polytostr (polymult n x y) "x" | y <- coeffs] | x <- coeffs]
+
+multtableSub :: Int -> [[Int]] -> [(Int,[Int])] -> [[String]]
+multtableSub n coeffs subs = [[polytostr (subsimp n (polymult n x y) subs) "x" | y <- coeffs] | x <- coeffs]
+
+-- represent a polynomial as a string, e.g. [1,2,1,3] -> 3x^3 + x^2 + 2x + 1
+-- terms with zero coefficients are removed (except in the case of the zero polynomial)
+-- unity coefficients are simplified to e.g. x, x^2 instead of 1x, 1x^2 
+polytostr :: [Int] -> String -> String
+polytostr p c = let l = length p
+                    z = 0 :: Int
+                    u = 1 :: Int
+                    cff x = if p!!x == 0 || (p!!x == 1 && x > 0) then "" else show (p!!x)
+                    zro x = cff x
+                    unt x = cff x ++ c
+                    pwr x = cff x ++ c ++ "^" ++ show x
+                    terms = [if p!!x == 0 then "" else if x == 0 then zro x else if x == 1 then unt x else pwr x | x <- [0..l-1]]
+                    fterms = filter (\x -> x /= "") terms
+                in if null fterms then "0" else foldr1 (\x a -> a ++ " + " ++ x) fterms
+
+prettify :: [[String]] -> String
+prettify mt = let tablines = intercalate "\t"
+              in unlines (map tablines mt)
+
+-- given a base, a polynomial, a second polynomial and a scale factor to apply to it before adding
+-- return the sum of the original polynomial and the scaled second poly mod the base
+addpoly :: Int -> [Int] -> Int -> [Int] -> [Int]
+addpoly n p t q = zipWith (\x y -> (x + t*y) `rem` n) p q
+
+-- replace the list element at index n with value v
+replaceAt :: Int -> [Int] -> Int -> [Int]
+replaceAt n xs v = let (f, s) = splitAt n xs
+                   in f ++ (v:tail s)
+
+-- make a single substitution mod n
+subf :: Int -> [Int] -> (Int, [Int]) -> [Int]
+subf n p (i, q) = let t = p!!i
+                      p' = replaceAt i p 0
+                  in addpoly n p' t q 
+
+-- given a polynomial and a list of index substitutions, return a simplified polynomial of lower degree
+-- for example, if the polynomial is [1,1,1,1,1] and the substitution list is
+-- [(4, [1,2,1,0,0]), (3, [2,1,1,0,0])] this function should replace the x^4 and x^3 terms with terms
+-- of lower degree, simplifying in the process mod n.
+subsimp :: Int -> [Int] -> [(Int, [Int])] -> [Int]
+subsimp n p [] = p
+subsimp n p (s:subs) = subsimp n (subf n p s) subs
+
+-- Substitutions for Pinter ch 27, ex C6. based on minimum polynomial x^3 + x^2 + 2
+subs :: [(Int, [Int])]
+subs = [(4, [2,1,1,0,0]), (3, [1,0,2,0,0])]
+
+base :: Int
+base = 3
+
+
+-- writeFile "mult table.txt" . prettify . multtableSub base coeff33 $ subs
 ------------
 --  DATA
 ------------
+
+-- All polynomials of degree 2 or lower over Z3
+coeff33 :: [[Int]]
+coeff33 = [[x,y,z] | z <- [0..2], y <- [0..2], x <- [0..2]]
 
 -- All polynomials of degree 2 or lower over Z5
 coeff53 :: [[Int]]
